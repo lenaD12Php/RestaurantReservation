88b98f79c7db57578f2d125363e0b0125e139593
Lena Damisi <Lena.damisi12@gmail.com>
2025-08-25 01:03:06 +0300

    Add MenuItem entity

---
+++ RestaurantReservation.Db/Entities/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantReservation.Db.Entities;
+
+public class MenuItem
+{
+    public MenuItem()
+    {
+        Orders = new List<Order>();
+    }
+
+    public int MenuItemId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public decimal Price { get; set; }
+
+    public Restaurant Restaurant { get; set; }
+    [Required]
+    public int RestaurantId { get; set; }
+    public List<Order> Orders { get; set; }
+}
