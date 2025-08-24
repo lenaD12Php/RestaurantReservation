b4f50d234fc84775573acb88f2031b8f8ac88466
Lena Damisi <Lena.damisi12@gmail.com>
2025-08-25 01:05:55 +0300

    Add Table entity

---
+++ RestaurantReservation.Db/Entities/Table.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantReservation.Db.Entities;
+
+public class Table
+{
+    public int TableId { get; set; }
+    [Required]
+    public int Capacity {  get; set; }
+
+    public Restaurant Restaurant { get; set; }
+    [Required]
+    public int RestaurantId { get; set; }
+}
