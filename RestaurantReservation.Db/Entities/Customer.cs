4667d4a09b0bbc6280a08d6f2fe815ff83b8e76e
Lena Damisi <Lena.damisi12@gmail.com>
2025-08-25 00:59:32 +0300

    Add Customer entity

---
+++ RestaurantReservation.Db/Entities/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantReservation.Db.Entities;
+
+public class Customer
+{
+    public Customer()
+    {
+        Reservations = new List<Reservation>();
+    }
+
+    public int CustomerId {  get; set; }
+    [Required]
+    public string Firstname { get; set; }
+    [Required]
+    public string Lastname { get; set; }
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [Phone]
+    public string PhoneNumber { get; set; }
+
+    public List<Reservation> Reservations { get; set; }
+}
