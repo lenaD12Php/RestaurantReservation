70326e6825665c58c5ca3802db941ff055a73341
Lena Damisi <Lena.damisi12@gmail.com>
2025-08-25 01:03:32 +0300

    Add Order entity

---
+++ RestaurantReservation.Db/Entities/Order.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantReservation.Db.Entities;
+
+public class Order
+{
+    public Order()
+    {
+        MenuItems = new List<MenuItem>();
+    }
+
+    public int OrderId { get; set; }
+    [Required]
+    public DateTime OrderDate { get; set; }
+    [Required]
+    public decimal TotalAmount { get; set; }
+
+    public Reservation Reservation { get; set; }
+    [Required]
+    public int ReservationId { get; set; }
+    public Employee Employee { get; set; }
+    [Required]
+    public int EmployeeId { get; set; }
+    public List<MenuItem> MenuItems { get; set; }
+}
