7d88b951e280e27d3ebce952962bf086176ae21e
Lena Damisi <Lena.damisi12@gmail.com>
2025-08-25 15:07:42 +0300

    Add enums for Capacity and Position

---
+++ RestaurantReservation.Db/Enums/Capacity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db.Enums
+{
+    internal class Capacity
+    {
+    }
+}
