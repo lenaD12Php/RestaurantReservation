212e52f5324594ad05d4dc846f41b061db4c8c0e
Lena Damisi <Lena.damisi12@gmail.com>
2025-08-25 16:43:24 +0300

    Seed tables with data

---
+++ RestaurantReservation.Db/Migrations/20250825133923_seedEntties.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedEntties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "customer_id", "Email", "first_name", "last_name", "phone_number" },
+                values: new object[,]
+                {
+                    { 1, "Lena12D@gmail.com", "Lena", "Damisi", "1234567890" },
+                    { 2, "JaberMuh@gmail.com", "Jaber", "Muhsen", "9834567892" },
+                    { 3, "HibaBashar@gmail.com", "Hiba", "Bashar", "1234568880" },
+                    { 4, "AishaKhaled@yahoo.com", "Aisha", "Khaled", "1279199990" },
+                    { 5, "AJaradat@gmail.com", "Aya", "Jaradat", "1267867890" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Restaurants",
+                columns: new[] { "restaurant_id", "Address", "Name", "opening_hours", "phone_number" },
+                values: new object[,]
+                {
+                    { 1, "Bahnhof Straße nr.1, Stade", "Köz", "Everyday 8-12am", "3389808990" },
+                    { 2, "Jenin old city", "Teen o Zaytoon", "Everyday 24/7", "0988765876" },
+                    { 3, "Nablus street 1", "DeliKitchen", "Friday and Saturday off,\n 9-12am S-T", "0889982348" },
+                    { 4, "Jarrar street", "Donuts Bites", "9-1am Everyday", "1236758889" },
+                    { 5, "Qabatiya cafe area", "Pizza House", "11am-11:59pm", "8972346757" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "employee_id", "first_name", "last_name", "Position", "restaurant_id" },
+                values: new object[,]
+                {
+                    { 1, "Sama", "Jamal", "Manager", 1 },
+                    { 2, "Zahraa", "Alwadi", "Manager", 2 },
+                    { 3, "Shams", "Eweis", "Server", 3 },
+                    { 4, "Ahlam", "Hilmi", "Cashier", 4 },
+                    { 5, "Sewar", "Anwar", "Chef", 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "MenuItems",
+                columns: new[] { "item_id", "Description", "Name", "Price", "restaurant_id" },
+                values: new object[,]
+                {
+                    { 1, "Chicken/Beef/Lamb with Rice spices and Veggies", "Kabseh", 30.50m, 1 },
+                    { 2, "Dough stuffed with diced onion, beef and dried mint, cooked in yogurt and served with rice", "Shishbarak", 30.00m, 2 },
+                    { 3, "Grape leaves stuffed with short-rice and veggies cooked with lots of lemon juice and olive oil", "Dawali", 15.50m, 3 },
+                    { 4, "Moist Chocolate cake with chocolate ganache and chocolate sauce", "Matilda chocolate cake slice", 10.00m, 4 },
+                    { 5, "Dough, Tomato Sauce , Cheese and Toppings of your choice", "Pizza", 35.50m, 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tables",
+                columns: new[] { "table_id", "Capacity", "restaurant_id" },
+                values: new object[,]
+                {
+                    { 1, 2, 1 },
+                    { 2, 4, 2 },
+                    { 3, 6, 3 },
+                    { 4, 2, 4 },
+                    { 5, 4, 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Reservations",
+                columns: new[] { "reservation_id", "customer_id", "party_size", "reservation_date", "restaurant_id", "table_id" },
+                values: new object[,]
+                {
+                    { 1, 1, 2, new DateTime(2025, 8, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 1 },
+                    { 2, 2, 2, new DateTime(2025, 7, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 2 },
+                    { 3, 1, 5, new DateTime(2025, 8, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, 3 },
+                    { 4, 3, 2, new DateTime(2025, 7, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), 4, 4 },
+                    { 5, 5, 6, new DateTime(2025, 8, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "order_id", "employee_id", "order_date", "reservation_id", "total_amount" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2025, 8, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 91.50m },
+                    { 2, 2, new DateTime(2025, 7, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 60.00m },
+                    { 3, 3, new DateTime(2025, 8, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, 77.50m },
+                    { 4, 3, new DateTime(2025, 8, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), 5, 108.50m },
+                    { 5, 4, new DateTime(2025, 7, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), 4, 40.00m }
+                });
+
+            migrationBuilder.InsertData(
+                table: "OrderItem",
+                columns: new[] { "order_item_id", "item_id", "order_id", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, 1, 1, 3 },
+                    { 2, 2, 2, 2 },
+                    { 3, 3, 3, 5 },
+                    { 4, 3, 4, 7 },
+                    { 5, 4, 5, 4 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "customer_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "employee_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "item_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "OrderItem",
+                keyColumn: "order_item_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "OrderItem",
+                keyColumn: "order_item_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "OrderItem",
+                keyColumn: "order_item_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "OrderItem",
+                keyColumn: "order_item_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "OrderItem",
+                keyColumn: "order_item_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Tables",
+                keyColumn: "table_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "item_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "item_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "item_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "item_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "order_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "order_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "order_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "order_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "order_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "restaurant_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "employee_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "employee_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "employee_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "employee_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Reservations",
+                keyColumn: "reservation_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Reservations",
+                keyColumn: "reservation_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Reservations",
+                keyColumn: "reservation_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Reservations",
+                keyColumn: "reservation_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Reservations",
+                keyColumn: "reservation_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "customer_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "customer_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "customer_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "customer_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Tables",
+                keyColumn: "table_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Tables",
+                keyColumn: "table_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Tables",
+                keyColumn: "table_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Tables",
+                keyColumn: "table_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "restaurant_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "restaurant_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "restaurant_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "restaurant_id",
+                keyValue: 4);
+        }
+    }
+}
--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RestaurantReservation.Db.Entities;
+using RestaurantReservation.Db.Enums;
 
 namespace RestaurantReservation.Db;
 
@@ -106,6 +107,9 @@ public class RestaurantReservationDbContext : DbContext
         modelBuilder.Entity<Employee>()
           .Property(e => e.Lastname)
           .HasColumnName("last_name");
+        modelBuilder.Entity<Employee>()
+            .Property(e => e.Position)
+            .HasConversion<string>();
 
         modelBuilder.Entity<MenuItem>()
             .Property(mi => mi.MenuItemId)
@@ -130,6 +134,356 @@ public class RestaurantReservationDbContext : DbContext
         modelBuilder.Entity<Restaurant>()
           .Property(r => r.OpeningHours)
           .HasColumnName("opening_hours");
+
+        var customers = new List<Customer>()
+        {
+            new Customer
+            {
+                CustomerId = 1,
+                Firstname = "Lena",
+                Lastname = "Damisi",
+                Email = "Lena12D@gmail.com",
+                PhoneNumber = "1234567890",
+            },
+            new Customer
+            {
+                CustomerId = 2,
+                Firstname = "Jaber",
+                Lastname = "Muhsen",
+                Email = "JaberMuh@gmail.com",
+                PhoneNumber = "9834567892",
+            },
+            new Customer
+            {
+                CustomerId = 3,
+                Firstname = "Hiba",
+                Lastname = "Bashar",
+                Email = "HibaBashar@gmail.com",
+                PhoneNumber = "1234568880",
+            },
+            new Customer
+            {
+                CustomerId = 4,
+                Firstname = "Aisha",
+                Lastname = "Khaled",
+                Email = "AishaKhaled@yahoo.com",
+                PhoneNumber = "1279199990"
+            },
+            new Customer
+            {
+                CustomerId = 5,
+                Firstname = "Aya",
+                Lastname = "Jaradat",
+                Email = "AJaradat@gmail.com",
+                PhoneNumber = "1267867890"
+            }
+        };
+        modelBuilder.Entity<Customer>().HasData(customers);
+
+        var restaurants = new List<Restaurant>()
+        {
+            new Restaurant()
+            {
+                RestaurantId = 1,
+                Name = "Köz",
+                Address = "Bahnhof Straße nr.1, Stade",
+                PhoneNumber = "3389808990",
+                OpeningHours = "Everyday 8-12am"
+            },
+            new Restaurant()
+            {
+                RestaurantId = 2,
+                Name = "Teen o Zaytoon",
+                Address = "Jenin old city",
+                PhoneNumber = "0988765876",
+                OpeningHours = "Everyday 24/7"
+            },
+            new Restaurant()
+            {
+                RestaurantId = 3,
+                Name = "DeliKitchen",
+                Address = "Nablus street 1",
+                PhoneNumber = "0889982348",
+                OpeningHours = "Friday and Saturday off,\n 9-12am S-T"
+            },
+            new Restaurant()
+            {
+                RestaurantId = 4,
+                Name = "Donuts Bites",
+                Address = "Jarrar street",
+                PhoneNumber = "1236758889",
+                OpeningHours = "9-1am Everyday"
+            },
+            new Restaurant()
+            {
+                RestaurantId = 5,
+                Name = "Pizza House",
+                Address = "Qabatiya cafe area",
+                PhoneNumber = "8972346757",
+                OpeningHours = "11am-11:59pm"
+            }
+        };
+        modelBuilder.Entity<Restaurant>().HasData(restaurants);
+
+        var tables = new List<Table>()
+        {
+            new Table()
+            {
+                TableId = 1,
+                Capacity = Capacity.Two,
+                RestaurantId = 1
+            },
+            new Table()
+            {
+                TableId = 2,
+                Capacity = Capacity.Four,
+                RestaurantId = 2
+            },
+            new Table()
+            {
+                TableId = 3,
+                Capacity = Capacity.Six,
+                RestaurantId = 3
+            },
+            new Table()
+            {
+                TableId = 4,
+                Capacity = Capacity.Two,
+                RestaurantId = 4
+            },
+            new Table()
+            {
+                TableId = 5,
+                Capacity = Capacity.Four,
+                RestaurantId = 5
+            }
+        };
+        modelBuilder.Entity<Table>().HasData(tables);
+
+        var reservations = new List<Reservation>()
+        {
+            new Reservation() 
+            {
+                ReservationId = 1,
+                RestaurantId = 1,
+                ReservationDate = new DateTime(2025,8,31),
+                PartySize = 2,
+                CustomerId = 1,
+                TableId = 1
+            },
+            new Reservation()
+            {
+                ReservationId = 2,
+                RestaurantId = 2,
+                ReservationDate = new DateTime(2025,7,2),
+                PartySize = 2,
+                CustomerId = 2,
+                TableId = 2
+            },
+            new Reservation()
+            {
+                ReservationId = 3,
+                RestaurantId = 3,
+                ReservationDate = new DateTime(2025,8,10),
+                PartySize = 5,
+                CustomerId = 1,
+                TableId = 3
+            },
+            new Reservation()
+            {
+                ReservationId = 4,
+                RestaurantId = 4,
+                ReservationDate = new DateTime(2025,7,11),
+                PartySize = 2,
+                CustomerId = 3,
+                TableId = 4
+            },
+            new Reservation()
+            {
+                ReservationId = 5,
+                RestaurantId = 3,
+                ReservationDate = new DateTime(2025,8,31),
+                PartySize = 6,
+                CustomerId = 5,
+                TableId = 3
+            }
+        };
+        modelBuilder.Entity<Reservation>().HasData(reservations);
+
+        var employees = new List<Employee>()
+        { 
+            new Employee
+            {
+                EmployeeId = 1, 
+                RestaurantId = 1,
+                Firstname = "Sama",
+                Lastname = "Jamal",
+                Position=Position.Manager
+            },
+            new Employee
+            {
+                EmployeeId = 2,
+                RestaurantId = 2,
+                Firstname = "Zahraa",
+                Lastname = "Alwadi",
+                Position=Position.Manager
+            },
+            new Employee
+            {
+                EmployeeId = 3,
+                RestaurantId = 3,
+                Firstname = "Shams",
+                Lastname = "Eweis",
+                Position = Position.Server
+            },
+            new Employee
+            {
+                EmployeeId = 4,
+                RestaurantId = 4,
+                Firstname = "Ahlam",
+                Lastname = "Hilmi",
+                Position = Position.Cashier
+            },
+            new Employee
+            {
+                EmployeeId = 5,
+                RestaurantId = 5,
+                Firstname = "Sewar",
+                Lastname = "Anwar",
+                Position = Position.Chef
+            }
+        };
+        modelBuilder.Entity<Employee>().HasData(employees);
+
+        var menuItems = new List<MenuItem>()
+        {
+            new MenuItem
+            {
+                MenuItemId = 1,
+                RestaurantId = 1,
+                Name = "Kabseh",
+                Description = "Chicken/Beef/Lamb with Rice spices and Veggies",
+                Price = 30.50m
+            },
+            new MenuItem
+            {
+                MenuItemId = 2,
+                RestaurantId = 2,
+                Name = "Shishbarak",
+                Description = "Dough stuffed with diced onion, beef and dried mint, cooked in yogurt and served with rice",
+                Price = 30.00m
+            },
+            new MenuItem
+            {
+                MenuItemId = 3,
+                RestaurantId = 3,
+                Name = "Dawali",
+                Description = "Grape leaves stuffed with short-rice and veggies cooked with lots of lemon juice and olive oil",
+                Price = 15.50m
+            },
+            new MenuItem
+            {
+                MenuItemId=4,
+                RestaurantId = 4,
+                Name = "Matilda chocolate cake slice",
+                Description = "Moist Chocolate cake with chocolate ganache and chocolate sauce",
+                Price = 10.00m
+            },
+            new MenuItem
+            {
+                MenuItemId = 5,
+                RestaurantId = 5,
+                Name = "Pizza",
+                Description = "Dough, Tomato Sauce , Cheese and Toppings of your choice",
+                Price = 35.50m
+            }
+        };
+        modelBuilder.Entity<MenuItem>().HasData(menuItems);
+
+        var orders = new List<Order>()
+        {
+            new Order
+            {
+                OrderId = 1,
+                ReservationId = 1,
+                EmployeeId = 1,
+                OrderDate = new DateTime(2025,8,31),
+                TotalAmount = 91.50m
+            },
+            new Order
+            {
+                OrderId = 2,
+                ReservationId = 2,
+                EmployeeId = 2,
+                OrderDate = new DateTime(2025,7,2),
+                TotalAmount = 60.00m
+            },
+            new Order
+            {
+                OrderId = 3,
+                ReservationId = 3,
+                EmployeeId = 3,
+                OrderDate = new DateTime(2025,8,10),
+                TotalAmount = 77.50m
+            },
+            new Order
+            {
+                OrderId=4,
+                ReservationId = 5,
+                EmployeeId = 3,
+                OrderDate = new DateTime(2025,8,31),
+                TotalAmount = 108.50m
+            },
+            new Order
+            {
+                OrderId=5,
+                ReservationId = 4,
+                EmployeeId = 4,
+                OrderDate = new DateTime(2025,7,11),
+                TotalAmount = 40.00m
+            }
+        };
+        modelBuilder.Entity<Order>().HasData(orders);
+
+        var OrderItems = new List<OrderItem>()
+        {
+            new OrderItem
+            {
+                OrderItemId = 1,
+                OrderId = 1,
+                MenuItemId = 1,
+                Quantity = 3
+            },
+            new OrderItem
+            {
+                OrderItemId = 2,
+                OrderId = 2,
+                MenuItemId = 2,
+                Quantity = 2
+            },
+            new OrderItem
+            {
+                OrderItemId = 3,
+                OrderId = 3,
+                MenuItemId = 3,
+                Quantity = 5
+            },
+            new OrderItem
+            {
+                OrderItemId = 4,
+                OrderId = 4,
+                MenuItemId = 3,
+                Quantity = 7
+            },
+            new OrderItem
+            {
+                OrderItemId = 5,
+                OrderId = 5,
+                MenuItemId = 4,
+                Quantity = 4
+            }
+        };
+        modelBuilder.Entity<OrderItem>().HasData(OrderItems);
     }
 }
 
