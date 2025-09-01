570675a3f7a83ae647c1270db1659059f5b8cc51
Lena Damisi <Lena.damisi12@gmail.com>
2025-09-01 22:32:51 +0300

    Add Order and MenuItem Navigation properties

--- RestaurantReservation.Db/Entities/OrderItem.cs
+++ RestaurantReservation.Db/Entities/OrderItem.cs
@@ -7,8 +7,10 @@ public class OrderItem
     public int OrderItemId {  get; set; }
     [Required]
     public int OrderId { get; set; }
+    public Order Order { get; set; }
     [Required]
     public int MenuItemId { get; set; }
+    public MenuItem MenuItem { get; set; }
     [Required]
     public int Quantity { get; set; }
 }
