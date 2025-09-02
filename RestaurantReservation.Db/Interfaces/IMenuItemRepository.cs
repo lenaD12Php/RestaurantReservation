72b0f8cc92ffdb2a2afd5a9d06bafc06c60d1bb4
Lena Damisi <Lena.damisi12@gmail.com>
2025-09-02 21:46:41 +0300

    Correct the name of  ListOrderedMenuItemsAsync in IMenuItemRepository and IMenuItemService

--- RestaurantReservation.Db/Interfaces/IMenuItemRepository.cs
+++ RestaurantReservation.Db/Interfaces/IMenuItemRepository.cs
@@ -7,7 +7,7 @@ public interface IMenuItemRepository
     Task AddMenuItemAsync(MenuItem menuItem);
     Task<MenuItem?> GetMenuItemByIdAsync(int MenuItemId);
     Task<List<MenuItem>> GetMenuItemsAsync();
-    Task<List<MenuItem>> ListOrderedMenuItems(int reservationId);
+    Task<List<MenuItem>> ListOrderedMenuItemsAsync(int reservationId);
     Task UpdateMenuItemAsync(MenuItem menuItem);
     Task DeleteMenuItemAsync(MenuItem menuItem);
 }
