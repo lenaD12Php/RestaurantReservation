3923c475a1717df867ae3c5c7614059f038f1e49
Lena Damisi <Lena.damisi12@gmail.com>
2025-09-02 20:37:58 +0300

    Add  ListOrdersAndMenuItemsAsync() in IOrderRepository and IOrderService

--- RestaurantReservation.Db/Interfaces/IOrderRepository.cs
+++ RestaurantReservation.Db/Interfaces/IOrderRepository.cs
@@ -6,6 +6,7 @@ public interface IOrderRepository
 {
     Task<Order?> GetOrderByIdAsync(int orderId);
     Task<List<Order>> GetOrdersAsync();
+    Task<List<Order>> ListOrdersAndMenuItemsAsync(int reservationId);
     Task AddOrderAsync(Order order);
     Task DeleteOrderAsync(Order order);
     Task UpdateOrderAsync(Order order);
--- RestaurantReservation.Db/Interfaces/IOrderService.cs
+++ RestaurantReservation.Db/Interfaces/IOrderService.cs
@@ -6,6 +6,7 @@ public interface IOrderService
 {
     Task<Order> GetOrderByIdAsync(int orderId);
     Task<List<Order>> GetOrdersAsync();
+    Task<List<Order>> ListOrdersAndMenuItemsAsync(int reservationId);
     Task AddOrderAsync(DateTime orderDate, decimal totalAmount, int reservationId, int employeeId);
     Task DeleteOrderAsync(int orderId);
     Task UpdateOrderOrderDateAsync(int  orderId, DateTime orderDate);
