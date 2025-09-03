8a7d9757d75593c5f784b0cb184b852ff7183dad
Lena Damisi <Lena.damisi12@gmail.com>
2025-09-03 20:25:58 +0300

    Add ReservationWithCustomerAndRestaurant View

---
+++ RestaurantReservation.Db/Migrations/20250903115650_AddReservationsWithCustomerAndRestaurantDetailsDatabaseView.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReservationsWithCustomerAndRestaurantDetailsDatabaseView : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql
+            (
+                @"Create view CustomerAndRestaurantByReservation
+                    as 
+                    select 
+                    res.reservation_id, 
+                    rest.restaurant_id, 
+                    rest.Name   as restaurant_name,
+                    rest.Address  as restaurant_address,
+                    rest.phone_number as restaurant_phone,
+                    res.reservation_date , 
+                    res.table_id,res.party_size, 
+                    c.customer_id, 
+                    c.first_name +' '+ c.last_name as customer_name, 
+                    c.phone_number as customer_phone
+                    from Reservations res
+                    left join Customers c  on  res.customer_id = c.customer_id
+                    left join Restaurants rest on res.restaurant_id = rest.restaurant_id;"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"drop view CustomerAndRestaurantByReservation");
+        }
+    }
+}
