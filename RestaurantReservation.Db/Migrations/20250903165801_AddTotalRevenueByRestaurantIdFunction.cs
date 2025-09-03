b843a1fecc1bb9b9e5e457c934c74ed5f56ae733
Lena Damisi <Lena.damisi12@gmail.com>
2025-09-03 20:46:40 +0300

    Add Total Revenue By restaurant function

---
+++ RestaurantReservation.Db/Migrations/20250903165801_AddTotalRevenueByRestaurantIdFunction.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTotalRevenueByRestaurantIdFunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @" create function fn_TotalRevenueForRestaurant
+                    (
+	                    @restaurantId INT
+                    )
+                        returns decimal(18,2)
+                        as 
+	                    begin
+
+	                    declare @totalRevenue decimal(18,2);
+
+	                    select @totalRevenue = Coalesce(sum(o.total_amount), 0.00)
+                        from Orders o
+                        inner join Reservations r on o.reservation_id = r.reservation_id
+                        where r.restaurant_id = @restaurantId;
+
+	                    return @totalRevenue;
+
+                    end;
+                ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"drop function dbo.fn_TotalRevenueForRestaurant;");
+        }
+    }
+}
