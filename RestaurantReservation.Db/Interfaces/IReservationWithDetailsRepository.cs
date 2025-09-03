f18239d92cd7301fbfb221a16ad13deb2caeae24
Lena Damisi <Lena.damisi12@gmail.com>
2025-09-03 21:21:07 +0300

    Add all methods needed to get Reservations With Customer And Restaurant Details for the database view

---
+++ RestaurantReservation.Db/Interfaces/IReservationWithDetailsRepository.cs
@@ -0,0 +1,8 @@
+﻿using RestaurantReservation.Db.Entities;
+
+namespace RestaurantReservation.Db.Interfaces;
+
+public interface IReservationWithDetailsRepository
+{
+    Task<List<CustomerAndRestaurantByReservation>> GetReservationsWithCustomerAndRestaurantDetailsAsync();
+}
---
+++ RestaurantReservation.Db/Repositories/ReservationWithDetailsRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Entities;
+using RestaurantReservation.Db.Interfaces;
+
+namespace RestaurantReservation.Db.Repositories;
+
+public class ReservationWithDetailsRepository : IReservationWithDetailsRepository
+{
+    private readonly RestaurantReservationDbContext _context;
+
+    public ReservationWithDetailsRepository(RestaurantReservationDbContext context) => _context = context;
+    
+
+    public async Task<List<CustomerAndRestaurantByReservation>> GetReservationsWithCustomerAndRestaurantDetailsAsync()
+    {
+        return await _context.ReservationsWithCustomerAndRestaurantDetails.AsNoTracking().ToListAsync();
+    }
+}
