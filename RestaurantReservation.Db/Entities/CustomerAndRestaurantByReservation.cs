678b3d1df04c14c490fe3092ffc959d450825386
Lena Damisi <Lena.damisi12@gmail.com>
2025-09-03 20:52:32 +0300

    Added entities for both customer and employee for the database view

---
+++ RestaurantReservation.Db/Entities/CustomerAndRestaurantByReservation.cs
@@ -0,0 +1,16 @@
+﻿namespace RestaurantReservation.Db.Entities;
+
+public sealed class CustomerAndRestaurantByReservation
+{
+    public int ReservationId { get; set; }
+    public int RestaurantId { get; set; }
+    public string RestaurantName { get; set; }
+    public string RestaurantAddress { get; set; }
+    public string RestaurantPhoneNumber { get; set; }
+    public DateTime ReservationDate { get; set; }
+    public int TableId { get; set; }
+    public int PartySize { get; set; }
+    public int CustomerId { get; set; }
+    public string CustomerName { get; set; }
+    public string CustomerPhoneNumber { get; set; }
+}
